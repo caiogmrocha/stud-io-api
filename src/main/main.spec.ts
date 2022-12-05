1748d291b4f7edce48e3f2d0a18a4be41e504e9a
caiogmrocha <caioguilherme739@gmail.com>
2022-12-05 20:56:44 -0300

    chore: add lint-staged

--- src/main/main.spec.ts
+++ src/main/main.spec.ts
@@ -1,4 +1,9 @@
+import log from './main';
+
 describe('Test With Husky', () => {
-    it('should be 2', () => expect(1 + 1).toBe(2));
+  it('should be 2', () => {
+    log('test')
+    expect(1 + 1).toBe(2)
+  });
 });
 
