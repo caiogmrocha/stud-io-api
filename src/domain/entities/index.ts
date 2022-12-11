cee3ecebba091f93741950b736759937109de16d
caiogmrocha <caioguilherme739@gmail.com>
2022-12-11 20:39:05 -0300

    feat: ensure Teacher entity will be created with correct props

--- src/domain/entities/index.ts
+++ src/domain/entities/index.ts
@@ -1,3 +1,4 @@
 export * from './entity';
 export * from './student';
-export * from './profile'
+export * from './profile';
+export * from './teacher';
---
+++ src/domain/entities/teacher.ts
@@ -0,0 +1,42 @@
+import { Entity, Profile } from ".";
+
+export interface ITeacherProps {
+    id: string;
+    name: string;
+    createdAt: Date;
+    updatedAt: Date;
+    deletedAt?: Date;
+    isDeleted: boolean;
+
+    profile: Profile;
+}
+
+export class Teacher extends Entity<ITeacherProps> {
+    constructor (props: ITeacherProps) {
+        super(props);
+    }
+
+    get id(): string {
+        return this.props.id;
+    }
+
+    get name(): string {
+        return this.props.name;
+    }
+
+    get createdAt(): Date {
+        return this.props.createdAt;
+    }
+
+    get updatedAt(): Date {
+        return this.props.updatedAt;
+    }
+
+    get deletedAt(): Date | undefined {
+        return this.props.deletedAt || undefined;
+    }
+
+    get isDeleted(): boolean {
+        return this.props.isDeleted;
+    }
+}
