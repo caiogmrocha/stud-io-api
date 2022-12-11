f42fd615016e85d07478b1d391f07d7a5e3458f3
caiogmrocha <caioguilherme739@gmail.com>
2022-12-11 20:55:16 -0300

    test: ensure Profile entity can have an owner being either teacher or student

--- src/domain/entities/profile.spec.ts
+++ src/domain/entities/profile.spec.ts
@@ -1,10 +1,10 @@
-import { Profile } from "."
+import { Profile, Student, Teacher } from "."
 
 import { faker } from '@faker-js/faker'
 
 describe('[Unit] Profile Entity', () => {
     it('should return with correct properties', () => {
-        const sut = new Profile({
+        const firstSut = new Profile({
             id: faker.datatype.uuid(),
             email: faker.internet.email(),
             password: faker.random.alphaNumeric(12),
@@ -14,9 +14,38 @@ describe('[Unit] Profile Entity', () => {
             updatedAt: new Date(),
             deletedAt: undefined,
             isDeleted: false,
+            owner: new Student({
+                id: faker.datatype.uuid(),
+                name: faker.name.fullName(),
+                createdAt: new Date(),
+                updatedAt: new Date(),
+                deletedAt: undefined,
+                isDeleted: false,
+                profile: undefined,
+            })
+        })
+        const secondSut = new Profile({
+            id: faker.datatype.uuid(),
+            email: faker.internet.email(),
+            password: faker.random.alphaNumeric(12),
+            level: Number(faker.random.numeric()),
+            type: faker.helpers.arrayElement([ 'student', 'teacher' ]),
+            createdAt: new Date(),
+            updatedAt: new Date(),
+            deletedAt: undefined,
+            isDeleted: false,
+            owner: new Teacher({
+                id: faker.datatype.uuid(),
+                name: faker.name.fullName(),
+                createdAt: new Date(),
+                updatedAt: new Date(),
+                deletedAt: undefined,
+                isDeleted: false,
+                profile: undefined,
+            })
         })
 
-        expect(sut.props).toEqual(expect.objectContaining({
+        expect(firstSut.props).toEqual(expect.objectContaining({
             id: expect.any(String),
             email: expect.any(String),
             password: expect.any(String),
@@ -26,6 +55,19 @@ describe('[Unit] Profile Entity', () => {
             updatedAt: expect.any(Date),
             deletedAt: undefined,
             isDeleted: false,
+            owner: expect.any(Student),
+        }))
+        expect(secondSut.props).toEqual(expect.objectContaining({
+            id: expect.any(String),
+            email: expect.any(String),
+            password: expect.any(String),
+            level: expect.any(Number),
+            type: expect.any(String),
+            createdAt: expect.any(Date),
+            updatedAt: expect.any(Date),
+            deletedAt: undefined,
+            isDeleted: false,
+            owner: expect.any(Teacher),
         }))
     })
 })
