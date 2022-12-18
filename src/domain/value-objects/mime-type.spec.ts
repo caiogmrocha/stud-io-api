618c06cbc8b83f12fe3e01d6fe53c83a3c219596
caiogmrocha <caioguilherme739@gmail.com>
2022-12-18 15:22:04 -0300

    feat: ensure MimeType value object accept valid values

--- src/domain/value-objects/mime-type.spec.ts
+++ src/domain/value-objects/mime-type.spec.ts
@@ -2,6 +2,34 @@ import { InvalidMimeType } from "./errors/invalid-mime-type"
 import { MimeType } from "./mime-type"
 
 describe('[Unit] Media Type Value Object', () => {
+  it('should accept valid media type', () => {
+    const sut1 = MimeType.create('text/plain');
+    const sut2 = MimeType.create('text/html');
+    const sut3 = MimeType.create('image/gif');
+    const sut4 = MimeType.create('image/png');
+    const sut5 = MimeType.create('image/jpeg');
+    const sut6 = MimeType.create('video/webm');
+    const sut7 = MimeType.create('video/ogg');
+    const sut8 = MimeType.create('application/pdf');
+
+    expect(sut1.isRight()).toBeTruthy()
+    expect(sut1.value).toBeInstanceOf(MimeType)
+    expect(sut2.isRight()).toBeTruthy()
+    expect(sut2.value).toBeInstanceOf(MimeType)
+    expect(sut3.isRight()).toBeTruthy()
+    expect(sut3.value).toBeInstanceOf(MimeType)
+    expect(sut4.isRight()).toBeTruthy()
+    expect(sut4.value).toBeInstanceOf(MimeType)
+    expect(sut5.isRight()).toBeTruthy()
+    expect(sut5.value).toBeInstanceOf(MimeType)
+    expect(sut6.isRight()).toBeTruthy()
+    expect(sut6.value).toBeInstanceOf(MimeType)
+    expect(sut7.isRight()).toBeTruthy()
+    expect(sut7.value).toBeInstanceOf(MimeType)
+    expect(sut8.isRight()).toBeTruthy()
+    expect(sut8.value).toBeInstanceOf(MimeType)
+  })
+
   it('should reject invalid media type', () => {
     const sut1 = MimeType.create('application')
     const sut2 = MimeType.create('html')
