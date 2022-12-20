eed76b4a0a221361540c374811b63a14f9f9b1d1
caiogmrocha <caioguilherme739@gmail.com>
2022-12-19 21:25:56 -0300

    test: ensure Email value object accepts valid values

--- src/domain/value-objects/email.spec.ts
+++ src/domain/value-objects/email.spec.ts
@@ -2,6 +2,13 @@ import { Email } from "./email"
 import { InvalidEmailError } from "./errors/invalid-email"
 
 describe('[Unit] Email Value Object', () => {
+  it('should accept valid e-mail', () => {
+    const sut = Email.create('email@email.com')
+
+    expect(sut.isRight()).toBeTruthy()
+    expect(sut.value).toBeInstanceOf(Email)
+  })
+
   it('should reject invalid e-mail', () => {
     const suts = [
       Email.create('test'),
