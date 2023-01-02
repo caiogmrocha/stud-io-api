52d47b33d62c69c6f319c3719dbb7967f82178c9
caiogmrocha <caioguilherme739@gmail.com>
2023-01-01 22:55:56 -0300

    feat: ensure MinimumValueValidator returns undefined if the provided value is valid

--- src/validation/rules/minimum-value-validator.spec.ts
+++ src/validation/rules/minimum-value-validator.spec.ts
@@ -2,6 +2,24 @@ import { MinimumValueError } from "../errors/minimum-value-error";
 import { MinimumValueValidator } from "./minimum-value-validator";
 
 describe('[Unit] Minimum Value Validator', () => {
+  it('should return undefined if the provided value is valid', async () => {
+    const suts = [
+      new MinimumValueValidator('string', 'value', 5).validate(),
+      new MinimumValueValidator('number', 10, 10).validate(),
+      new MinimumValueValidator('array', [1, 2, 3], 3).validate(),
+      new MinimumValueValidator('object', {
+        prop1: 1,
+        prop2: 2,
+        prop3: 3,
+      }, 3).validate(),
+    ];
+
+    for await (const result of suts) {
+      expect(result.isRight()).toBeTruthy();
+      expect(result.value).toBeUndefined();
+    }
+  });
+
   it('should return MinimumValueError if the provided value is invalid', async () => {
     const suts = [
       new MinimumValueValidator('string', 'value', 6).validate(),
