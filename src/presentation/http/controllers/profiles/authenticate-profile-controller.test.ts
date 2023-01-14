da2748a1ee27c21b18c5170a2a60505c617f4c94
caiogmrocha <caioguilherme739@gmail.com>
2023-01-14 20:10:40 -0300

    test: ensure AuthenticateProfileController returns 401 if the provided e-mail and password match with some profile in data source

--- src/presentation/http/controllers/profiles/authenticate-profile-controller.test.ts
+++ src/presentation/http/controllers/profiles/authenticate-profile-controller.test.ts
@@ -1,8 +1,10 @@
+import { app } from '@/main/server';
+import { ValidationCompositeError } from '@/validation/errors/validation-composite-error';
+
 import { faker } from '@faker-js/faker';
 import { prisma } from '@/infra/prisma/prisma';
 import request from 'supertest';
-import { app } from '@/main/server';
-import { ValidationCompositeError } from '@/validation/errors/validation-composite-error';
+import { ProfileDoesNotExistsError } from '@/app/services/profiles/errors/profile-does-not-exists-error';
 
 describe('[E2E] Authenticate Profile Controller', () => {
   let sharedData;
@@ -24,6 +26,20 @@ describe('[E2E] Authenticate Profile Controller', () => {
         },
       },
     });
+  });
+
+  it('should return 401 if the provided e-mail and password match with some profile in data source', async () => {
+    const response = await request(app).post('/profiles/login').send({
+      email: 'random@email.com',
+      password: '123456789101',
+    });
+
+    expect(response.status).toBe(401);
+    expect(response.body).toEqual(expect.objectContaining({
+      error: expect.objectContaining({
+        name: ProfileDoesNotExistsError.name,
+      }),
+    }));
   });
 
   it('should return 422 if the provided data is invalid', async () => {
