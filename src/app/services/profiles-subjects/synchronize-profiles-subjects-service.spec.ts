27d0f6d3a2ab69146747b5316f34275e22172d4f
caiogmrocha <caioguilherme739@gmail.com>
2023-04-13 19:41:05 -0300

    test: ensure SynchronizeProfilesSubjectsService returns SubjectsDoesNotExistsError if at least one subject has not been founded

--- src/app/services/profiles-subjects/synchronize-profiles-subjects-service.spec.ts
+++ src/app/services/profiles-subjects/synchronize-profiles-subjects-service.spec.ts
@@ -1,3 +1,4 @@
+import { ISynchronizeProfilesSubjectsUseCaseInputBoundary } from "@/domain/usecases/profiles-subjects/i-synchronize-profiles-subjects-use-case";
 import { SynchronizeProfilesSubjectsService } from "./synchronize-profiles-subjects-service";
 import { InMemoryGetProfilesRepository } from "@/../tests/mocks/infra/in-memory/profiles/in-memory-get-profiles-repository";
 import { InMemoryGetSubjectsRepository } from "@/../tests/mocks/infra/in-memory/subjects/in-memory-get-subjects-repository";
@@ -5,10 +6,10 @@ import { InMemoryGetProfilesSubjectsRepository } from "@/../tests/mocks/infra/in
 import { InMemoryCreateProfileSubjectRepository } from "@/../tests/mocks/infra/in-memory/profiles-subjects/in-memory-create-profile-subject-repository";
 import { InMemoryDeleteProfileSubjectRepository } from "@/../tests/mocks/infra/in-memory/profiles-subjects/in-memory-delete-profile-subject-repository";
 import { ProfilesDoesNotExistsError } from "../profiles/errors/profiles-does-not-exists-error";
+import { SubjectsDoesNotExistsError } from "../subjects/errors/subjects-does-not-exists-error";
 
 import { faker } from "@faker-js/faker";
 import { setupInMemoryDatabase } from "@/../tests/helpers/in-memory-database";
-import { ISynchronizeProfilesSubjectsUseCaseInputBoundary } from "@/domain/usecases/profiles-subjects/i-synchronize-profiles-subjects-use-case";
 
 type SutTypes = {
 	sut: SynchronizeProfilesSubjectsService;
@@ -32,7 +33,7 @@ function makeSut(): SutTypes {
 }
 
 describe('[Unit] Synchronize Profiles Subjects Service', () => {
-	it('should return ProfileDoesNotExistsError if at least one profile has not been founded', async () => {
+	it('should return ProfilesDoesNotExistsError if at least one profile has not been founded', async () => {
 		const profileId = faker.datatype.uuid();
 		const profileIdThatDoesNotExists = faker.datatype.uuid();
 		const subjectId = faker.datatype.number();
@@ -88,9 +89,61 @@ describe('[Unit] Synchronize Profiles Subjects Service', () => {
 		expect(output.value).toBeInstanceOf(ProfilesDoesNotExistsError);
 	});
 
-	// it('should return ProfileDoesNotExistsError if at least one subject has not been founded', async () => {
+	it('should return SubjectsDoesNotExistsError if at least one subject has not been founded', async () => {
+		const subjectId = faker.datatype.number();
+		const subjectIdThatDoesNotExists = faker.datatype.number();
+		const profileId = faker.datatype.uuid();
 
-	// });
+		await setupInMemoryDatabase({
+			profiles: [
+				{
+					id: profileId,
+					email: faker.internet.email(),
+					password: faker.random.alphaNumeric(12),
+					type: 'teacher',
+					level: 12,
+					created_at: new Date(),
+					is_deleted: false,
+				}
+			],
+			students: [],
+			teachers: [],
+			subjects: [
+				{
+					id: subjectId,
+					name: faker.name.fullName(),
+					display_name: faker.name.fullName(),
+					description: faker.random.words(6),
+					is_deleted: false,
+					created_at: new Date(),
+				}
+			],
+			profile_subjects: [
+				{
+					profile_id: profileId,
+					subject_id: subjectId,
+					created_at: new Date(),
+				}
+			],
+		});
+
+		const { sut } = makeSut();
+
+		const input: ISynchronizeProfilesSubjectsUseCaseInputBoundary = {
+			profilesIds: [
+				profileId,
+			],
+			subjectsIds: [
+				subjectId,
+				subjectIdThatDoesNotExists,
+			]
+		};
+
+		const output = await sut.execute(input);
+
+		expect(output.isLeft()).toBeTruthy();
+		expect(output.value).toBeInstanceOf(SubjectsDoesNotExistsError);
+	});
 
 	// it('should be able to synchronize the profiles with the subjects', async () => {
 
