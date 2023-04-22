61b73924df6148616727d1537782d246a6c4b746
caiogmrocha <caioguilherme739@gmail.com>
2023-04-22 19:34:16 -0300

    chore: create add_cascade_to_on_delete_trigger_to_all_tables migration

--- src/presentation/http/contracts/errors/index.ts
+++ src/presentation/http/contracts/errors/index.ts
@@ -1,2 +1,5 @@
 export * from './internal-server-error';
 export * from './unauthorized-error';
+export * from './bad-request-error';
+export * from './forbidden-error';
+export * from './not-found-error';
