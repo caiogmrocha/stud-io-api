76f582f7a179b7a61c6fb9288b4a79e6cab63ba4
caiogmrocha <caioguilherme739@gmail.com>
2023-04-22 19:34:43 -0300

    test: ensure UpdateProfileRegistrationController returns 404 if profile does not exists

--- src/presentation/http/controllers/profiles/update-profile-registration-controller.test.ts
+++ src/presentation/http/controllers/profiles/update-profile-registration-controller.test.ts
@@ -1,4 +1,4 @@
-import { UnauthorizedError } from '../../contracts';
+import * as Http from '../../contracts';
 import { app } from '@/main/server';
 
 import { prisma } from '@/infra/prisma/prisma';
@@ -6,6 +6,49 @@ import { faker } from '@faker-js/faker';
 import request from 'supertest';
 
 describe('[E2E] Update Profile Registration Controller', () => {
+	it('should return 404 if profile does not exists', async () => {
+		const email = faker.internet.email();
+		const password = faker.random.alphaNumeric(12);
+
+		const { id: profileId } = await prisma.profile.create({
+			data: {
+				email,
+				password,
+				type: 'student',
+				student: {
+					create: {
+						name: faker.name.fullName(),
+					},
+				},
+			},
+		});
+
+		const profileAuthenticationResponse = await request(app)
+			.post('/profiles/login')
+			.send({ email, password });
+
+		await prisma.profile.delete({
+			where: {
+				id: profileId,
+			},
+		});
+
+		const updateProfileRegistrationResponse = await request(app)
+			.put(`/profiles`)
+			.send({
+				name: faker.name.fullName(),
+				subjects: [],
+			})
+			.set('Authorization', profileAuthenticationResponse.body.token)
+
+		expect(updateProfileRegistrationResponse.status).toBe(404);
+		expect(updateProfileRegistrationResponse.body).toEqual(expect.objectContaining({
+			error: expect.objectContaining({
+				name: Http.NotFoundError.name,
+			}),
+		}));
+	});
+
 	it('should return 401 if profile is not authenticated', async () => {
 		await prisma.profile.create({
 			data: {
@@ -30,7 +73,7 @@ describe('[E2E] Update Profile Registration Controller', () => {
 		expect(updateProfileRegistrationResponse.status).toBe(401);
 		expect(updateProfileRegistrationResponse.body).toEqual(expect.objectContaining({
 			error: expect.objectContaining({
-				name: UnauthorizedError.name,
+				name: Http.UnauthorizedError.name,
 			}),
 		}));
 	});
