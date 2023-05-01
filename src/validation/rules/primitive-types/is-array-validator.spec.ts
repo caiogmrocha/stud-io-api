b235c41dfde2494f8eba76b65f8521b6bf765ef9
caiogmrocha <caioguilherme739@gmail.com>
2023-05-01 12:55:50 -0300

    test: ensure IsArrayValidator accept one parameter as a callback to validate the array items

--- src/validation/rules/primitive-types/is-array-validator.spec.ts
+++ src/validation/rules/primitive-types/is-array-validator.spec.ts
@@ -1,7 +1,25 @@
 import { IsArrayValidatorError } from "@/validation/errors/primitive-types/is-array-validator-error";
 import { IsArrayValidator } from "./is-array-validator";
+import { ValueInListValidator } from "../value-in-list-validator";
+import { ValidationComposite } from "@/validation/validation-composite";
+import { ValidationCompositeError } from "@/validation/errors/validation-composite-error";
 
 describe('[Unit] Is Array Validator', () => {
+	it('should accept one parameter as a callback to validate the array items', async () => {
+		const sut1 = await new IsArrayValidator('anyField', ['any_text']).validate((fieldName, fieldValue) => {
+			return new ValidationComposite([
+				new ValueInListValidator(fieldName, fieldValue, ['other_text']),
+			]);
+		});
+
+		const sut2 = await new IsArrayValidator('anyField', ['any_text']).validate();
+
+		expect(sut1.isLeft()).toBeTruthy();
+		expect(sut1.value).toBeInstanceOf(ValidationCompositeError);
+		expect(sut2.isRight()).toBeTruthy();
+		expect(sut2.value).toBeUndefined();
+	});
+
 	it('should return undefined if the provided value is valid', async () => {
 		const suts = [
 			new IsArrayValidator('anyField', ['any_text']).validate(),
