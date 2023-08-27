be86fcb8de5d4decb0d0e4d9226afab500f87eb0
caiogmrocha <caioguilherme739@gmail.com>
2023-08-27 19:49:02 -0300

    test: ensure SendCodeToProfileEmailService returns 6 digits code and the authorization token if e-mail exists in data source

--- src/app/services/profiles/password-recovery/send-code-to-profile-email-service.spec.ts
+++ src/app/services/profiles/password-recovery/send-code-to-profile-email-service.spec.ts
@@ -1,29 +1,92 @@
+import { IProfileModel } from "@/app/contracts/repositories/profiles/i-profile-model";
 import { InMemoryGetProfilesRepository } from "../../../../../tests/mocks/infra/in-memory/profiles/in-memory-get-profiles-repository";
 import { ProfileDoesNotExistsError } from "../errors/profile-does-not-exists-error";
 import { SendCodeToProfileEmailService } from "./send-code-to-profile-email-service";
-
-type SutTypes = {
-	sut: SendCodeToProfileEmailService;
-}
-
-function makeSut(): SutTypes {
-	const getProfilesRepository = new InMemoryGetProfilesRepository();
-	const sut = new SendCodeToProfileEmailService(
-		getProfilesRepository,
-	);
-
-	return { sut };
-}
+import { IGetProfilesRepository } from "@/app/contracts/repositories/profiles/i-get-profiles-repository";
+import { ICreatePasswordRecoveryRequestRepository } from "@/app/contracts/repositories/passwords-recoveries/i-create-password-recovery-request-repository";
+import { IPasswordRecoveryModel } from "@/app/contracts/repositories/passwords-recoveries/i-password-recovery-model";
+import { IJWTAuthenticationProvider } from "@/app/contracts/auth/jwt/i-jwt-authentication-provider";
+import { JWTAuthenticationProvider } from "@/infra/jwt/jwt-authentication-provider";
 
 describe('[Unit] SendCodeToProfileEmailService', () => {
 	it('should return ProfileDoesNotExistsError if the provided e-mail does not exists in data source', async () => {
-		const { sut } = makeSut();
+		const getProfilesRepository = {
+			get: jest.fn().mockResolvedValue([]),
+		} as InMemoryGetProfilesRepository;
+
+		const createPasswordRecoveryRequestRepository = {
+			createPasswordRecoveryRegister: jest.fn()
+		} as ICreatePasswordRecoveryRequestRepository;
+
+		const fakeJwtAuthenticationProvider = {
+			sign: jest.fn(),
+			verify: jest.fn(),
+		} as IJWTAuthenticationProvider
+
+		const sut = new SendCodeToProfileEmailService(
+			getProfilesRepository,
+			createPasswordRecoveryRequestRepository,
+			fakeJwtAuthenticationProvider,
+		);
 
 		const result = await sut.execute({
-			email: 'any_email',
+			email: 'any@email.com',
 		});
 
 		expect(result.isLeft()).toBeTruthy();
 		expect(result.value).toBeInstanceOf(ProfileDoesNotExistsError);
+	});
+
+	it('should return the 6 digits code and authorization token if the e-mail exists in data source', async () => {
+		const getProfilesRepository = {
+			get: jest.fn().mockResolvedValue([
+				{
+					id: 'any-id',
+					email: 'any@email.com',
+					password: 'any-password',
+					created_at: new Date(),
+					updated_at: new Date(),
+					is_deleted: false,
+					level: 0,
+					type: 'student',
+				}
+			] as IProfileModel[]),
+		} as unknown as IGetProfilesRepository;
+
+		const createPasswordRecoveryRequestRepository = {
+			createPasswordRecoveryRegister: jest.fn()
+		} as unknown as ICreatePasswordRecoveryRequestRepository;
+
+		const fakeJwtAuthenticationProvider = {
+			sign: jest.fn().mockResolvedValue({
+				isLeft: () => false,
+				isRight: () => true,
+				value: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyX2lkIjoxMjMsInVzZXJuYW1lIjoiam9obiIsImVtYWlsIjoiam9obkBleGFtcGxlLmNvbSIsImV4cCI6MTYzMTk3MzAwMH0.yQy-b2hPWACy_Ldj0T49ynBe6XhG4F3Rr4lSmlzEtBo'
+			} as Awaited<ReturnType<IJWTAuthenticationProvider['sign']>>),
+			verify: jest.fn(),
+		} as IJWTAuthenticationProvider
+
+		const sut = new SendCodeToProfileEmailService(
+			getProfilesRepository,
+			createPasswordRecoveryRequestRepository,
+			fakeJwtAuthenticationProvider,
+		);
+
+		const result = await sut.execute({
+			email: 'any@email.com',
+		});
+
+		expect(result.isRight()).toBeTruthy();
+		expect(result.value).toEqual(expect.objectContaining({
+			code: expect.any(String),
+			token: expect.any(String),
+		}));
+		expect(fakeJwtAuthenticationProvider.sign).toHaveBeenCalled();
+		expect(createPasswordRecoveryRequestRepository.createPasswordRecoveryRegister).toHaveBeenCalledWith({
+			profile_id: 'any-id',
+			code: expect.any(String),
+			send_code_token: expect.any(String),
+			expires_at: expect.any(Date),
+		});
 	});
 });
