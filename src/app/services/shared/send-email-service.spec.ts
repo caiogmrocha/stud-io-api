9e217c9d85ced936150f669f817883a1d23c29e8
caiogmrocha <caioguilherme739@gmail.com>
2023-09-10 22:51:38 -0300

    test: ensure SendEmailService returns SendEmailServiceError if SendEmailProvider throws an error

---
+++ src/app/services/shared/send-email-service.spec.ts
@@ -0,0 +1,34 @@
+import { faker } from "@faker-js/faker";
+import { SendEmailService } from "./send-email-service";
+import { SendEmailServiceError } from "./errors/send-email-service-error";
+import { ISendEmailProvider } from "@/app/contracts/email/i-send-email-provider";
+import { left, right } from "@/utils/logic/either";
+import { CanNotRenderTemplateError } from "@/app/contracts/template/errors/can-not-render-template-error";
+
+describe('[Unit] SendEmailService', () => {
+	it('should return an SendEmailServiceError if SendEmailProvider throws an error', async () => {
+		const fakeTemplateProvider = {
+			render: jest.fn().mockResolvedValue(right(undefined)),
+		};
+
+		const fakeSendEmailProvider = {
+			send: jest.fn().mockResolvedValue(left(new CanNotRenderTemplateError('any-error'))),
+		} as ISendEmailProvider;
+
+		const sut = new SendEmailService(
+			fakeTemplateProvider,
+			fakeSendEmailProvider,
+		);
+
+		const output = await sut.execute({
+			to: faker.internet.email(),
+			from: faker.internet.email(),
+			subject: faker.random.words(5),
+			templatePath: 'any-path',
+			templateData: {},
+		});
+
+		expect(output.isLeft()).toBeTruthy();
+		expect(output.value).toBeInstanceOf(SendEmailServiceError);
+	});
+});
