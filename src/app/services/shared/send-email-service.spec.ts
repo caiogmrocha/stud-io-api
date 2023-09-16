8c629fe523b9bb76be596f0a27312c4edf634928
caiogmrocha <caioguilherme739@gmail.com>
2023-09-16 17:46:28 -0300

    test: ensure SendEmailService returns SendEmailServiceError if template if templateProvider throws an error

--- src/app/services/shared/send-email-service.spec.ts
+++ src/app/services/shared/send-email-service.spec.ts
@@ -4,15 +4,16 @@ import { SendEmailServiceError } from "./errors/send-email-service-error";
 import { ISendEmailProvider } from "@/app/contracts/email/i-send-email-provider";
 import { left, right } from "@/utils/logic/either";
 import { CanNotRenderTemplateError } from "@/app/contracts/template/errors/can-not-render-template-error";
+import { CanNotSendEmailError } from "@/app/contracts/email/errors/can-not-send-email-error";
 
 describe('[Unit] SendEmailService', () => {
-	it('should return an SendEmailServiceError if SendEmailProvider throws an error', async () => {
+	it('should return an SendEmailServiceError if TemplateProvider throws an error', async () => {
 		const fakeTemplateProvider = {
-			render: jest.fn().mockResolvedValue(right(undefined)),
+			render: jest.fn().mockResolvedValue(left(new CanNotRenderTemplateError('any-error'))),
 		};
 
 		const fakeSendEmailProvider = {
-			send: jest.fn().mockResolvedValue(left(new CanNotRenderTemplateError('any-error'))),
+			send: jest.fn().mockResolvedValue(right(undefined)),
 		} as ISendEmailProvider;
 
 		const sut = new SendEmailService(
@@ -32,6 +33,31 @@ describe('[Unit] SendEmailService', () => {
 		expect(output.value).toBeInstanceOf(SendEmailServiceError);
 	});
 
-	it.todo('should return an SendEmailServiceError if TemplateProvider throws an error');
+	it('should return an SendEmailServiceError if SendEmailProvider throws an error', async () => {
+		const fakeTemplateProvider = {
+			render: jest.fn().mockResolvedValue(right(undefined)),
+		};
+
+		const fakeSendEmailProvider = {
+			send: jest.fn().mockResolvedValue(left(new CanNotSendEmailError('any-error'))),
+		} as ISendEmailProvider;
+
+		const sut = new SendEmailService(
+			fakeTemplateProvider,
+			fakeSendEmailProvider,
+		);
+
+		const output = await sut.execute({
+			to: faker.internet.email(),
+			from: faker.internet.email(),
+			subject: faker.random.words(5),
+			templatePath: 'any-path',
+			templateData: {},
+		});
+
+		expect(output.isLeft()).toBeTruthy();
+		expect(output.value).toBeInstanceOf(SendEmailServiceError);
+	});
+
 	it.todo('should return undefined if SendEmailProvider has sent the e-mail with success');
 });
