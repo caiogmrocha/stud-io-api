2ef434173ae282d3c5ad3db72f4b87a899d00796
caiogmrocha <caioguilherme739@gmail.com>
2023-09-17 13:03:11 -0300

    chore: update environment variables

--- src/utils/env.ts
+++ src/utils/env.ts
@@ -5,13 +5,18 @@ import { z } from "zod";
 dotenv.config({ path: path.resolve(__dirname, '..', '..', '.env') });
 
 const envSchema = z.object({
-	DATABASE_URL: z.string().nonempty().regex(/([a-z]+):\/\/([a-z]+):([a-z]+)@([a-z]+):([a-z]+)\/([a-z]+)/),
+	DATABASE_URL: z.string().nonempty().url(),
 	HTTP_HOST: z.string().nonempty().ip(),
 	HTTP_PORT: z.string().nonempty().regex(/[0-9]+/).transform(Number),
 	JWT_SECRET: z.string().nonempty(),
 	REDIS_HOST: z.string().nonempty().ip(),
 	REDIS_PORT: z.string().nonempty().regex(/[0-9]+/).transform(Number),
+	SMTP_HOST: z.string().nonempty(),
+	SMTP_PORT: z.string().nonempty().regex(/[0-9]+/).transform(Number),
+	SMTP_USER: z.string().nonempty(),
+	SMTP_PASS: z.string().nonempty(),
 })
+
 
 export const env = envSchema.parse({
 	DATABASE_URL: process.env.DATABASE_URL,
@@ -20,4 +25,8 @@ export const env = envSchema.parse({
 	JWT_SECRET: process.env.JWT_SECRET,
 	REDIS_HOST: process.env.REDIS_HOST,
 	REDIS_PORT: process.env.REDIS_PORT,
+	SMTP_HOST: process.env.SMTP_HOST,
+	SMTP_PORT: process.env.SMTP_PORT,
+	SMTP_USER: process.env.SMTP_USER,
+	SMTP_PASS: process.env.SMTP_PASS,
 });
