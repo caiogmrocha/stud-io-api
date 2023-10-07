4e198349e06857f7fc31ba58f3af9932c81f2605
caiogmrocha <caioguilherme739@gmail.com>
2023-10-07 13:50:12 -0300

    ci: update

--- src/utils/env.ts
+++ src/utils/env.ts
@@ -21,20 +21,7 @@ const envSchema = z.object({
 	SMTP_PORT: z.string().nonempty().regex(/[0-9]+/).transform(Number),
 	SMTP_USER: z.string().nonempty(),
 	SMTP_PASS: z.string().nonempty(),
-})
-
-console.log({
-	DATABASE_URL: process.env.DATABASE_URL,
-	HTTP_HOST: process.env.HTTP_HOST,
-	HTTP_PORT: process.env.HTTP_PORT,
-	JWT_SECRET: process.env.JWT_SECRET,
-	REDIS_HOST: process.env.REDIS_HOST,
-	REDIS_PORT: process.env.REDIS_PORT,
-	SMTP_HOST: process.env.SMTP_HOST,
-	SMTP_PORT: process.env.SMTP_PORT,
-	SMTP_USER: process.env.SMTP_USER,
-	SMTP_PASS: process.env.SMTP_PASS,
-})
+});
 
 export const env = envSchema.parse({
 	DATABASE_URL: process.env.DATABASE_URL,
