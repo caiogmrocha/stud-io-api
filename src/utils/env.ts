be0581c8926ebcb8639f7090129635d555c9056b
caiogmrocha <caioguilherme739@gmail.com>
2023-10-07 12:44:20 -0300

    ci: update

--- src/utils/env.ts
+++ src/utils/env.ts
@@ -2,13 +2,7 @@ import path from "path";
 import dotenv from "dotenv";
 import { z } from "zod";
 
-if (process.env.NODE_ENV !== 'test') {
-	dotenv.config({ path: path.resolve(__dirname, '..', '..', '.env') });
-} else {
-	dotenv.config({ path: path.resolve(__dirname, '..', '..', '.env.test') });
-}
-
-console.log('process.env.NODE_ENV', process.env.NODE_ENV)
+dotenv.config();
 
 const envSchema = z.object({
 	DATABASE_URL: z.string().nonempty().url(),
@@ -24,7 +18,7 @@ const envSchema = z.object({
 })
 
 
-export const env = {
+export const env = envSchema.parse({
 	DATABASE_URL: process.env.DATABASE_URL,
 	HTTP_HOST: process.env.HTTP_HOST,
 	HTTP_PORT: process.env.HTTP_PORT,
@@ -35,17 +29,4 @@ export const env = {
 	SMTP_PORT: process.env.SMTP_PORT,
 	SMTP_USER: process.env.SMTP_USER,
 	SMTP_PASS: process.env.SMTP_PASS,
-}
-
-// export const env = envSchema.parse({
-// 	DATABASE_URL: process.env.DATABASE_URL,
-// 	HTTP_HOST: process.env.HTTP_HOST,
-// 	HTTP_PORT: process.env.HTTP_PORT,
-// 	JWT_SECRET: process.env.JWT_SECRET,
-// 	REDIS_HOST: process.env.REDIS_HOST,
-// 	REDIS_PORT: process.env.REDIS_PORT,
-// 	SMTP_HOST: process.env.SMTP_HOST,
-// 	SMTP_PORT: process.env.SMTP_PORT,
-// 	SMTP_USER: process.env.SMTP_USER,
-// 	SMTP_PASS: process.env.SMTP_PASS,
-// });
+});
