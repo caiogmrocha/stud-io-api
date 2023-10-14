db846604aa73a5cdfc8d39ce143d58d88c5c06a0
caiogmrocha <caioguilherme739@gmail.com>
2023-10-14 19:19:28 -0300

    test: ensure ChangePasswordService return void if the password is changed successfully

--- src/app/services/profiles/password-recovery/change-password-service.spec.ts
+++ src/app/services/profiles/password-recovery/change-password-service.spec.ts
@@ -5,19 +5,27 @@ import { faker } from '@faker-js/faker';
 import { JWTVerifyError } from "@/app/contracts/auth/jwt/errors/jwt-verify-error";
 import { ChangePasswordService } from "./change-password-service";
 import { JWTAuthenticationProvider } from "@/infra/jwt/jwt-authentication-provider";
+import { IUpdateProfileRepository } from '@/app/contracts/repositories/profiles/i-update-profile-repository';
 
 describe('[Unit] ChangePasswordService', () => {
 	it('should return JWTVerifyError if token is invalid', async () => {
 		// Arrange
 		const jwtAutheticationProvider = new JWTAuthenticationProvider();
+		const fakeProfileRepository = {
+			update: jest.fn(),
+		} as IUpdateProfileRepository
 
+		const sut = new ChangePasswordService(
+			fakeProfileRepository,
+			jwtAutheticationProvider,
+		);
+
+		// Act
 		const token = await jwtAutheticationProvider.sign({
 			id: crypto.randomUUID(),
 			email: faker.internet.email(),
 		}, -1);
-		const sut = new ChangePasswordService(jwtAutheticationProvider);
 
-		// Act
 		const result = await sut.execute({
 			password: 'any_password',
 			token: token.value as string,
@@ -28,5 +36,36 @@ describe('[Unit] ChangePasswordService', () => {
 		expect(result.value).toBeInstanceOf(JWTVerifyError);
 	});
 
-	it.todo('should return void if the password is changed successfully');
+	it('should return void if the password is changed successfully', async () => {
+		// Arrange
+		const profileEmail = faker.internet.email();
+		const profileId = crypto.randomUUID();
+		const jwtAutheticationProvider = new JWTAuthenticationProvider();
+		const fakeProfileRepository = {
+			update: jest.fn(),
+		} as IUpdateProfileRepository
+
+		const sut = new ChangePasswordService(
+			fakeProfileRepository,
+			jwtAutheticationProvider,
+			);
+
+		// Act
+		const token = await jwtAutheticationProvider.sign({
+			id: profileId,
+			email: profileEmail,
+		}, 3 * 60 * 60 * 1000);
+
+		const result = await sut.execute({
+			password: 'any_password',
+			token: token.value as string,
+		});
+
+		// Assert
+		expect(result.isRight()).toBeTruthy();
+		expect(result.value).toBeUndefined();
+		expect(fakeProfileRepository.update).toBeCalledWith(expect.any(String), {
+			password: 'any_password',
+		});
+	});
 });
